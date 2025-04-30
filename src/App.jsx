cbc301329381a9aa2850e97dbb9838cec22976db
Luis Diaz <lediaz680@misena.edu.co>
2025-04-30 10:14:22 -0500

    Inicializando React + Vite Con Three.js Setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+	return (
+	  <div style={{ width: '100vw', height: '100vh' }}>
+		<h1>3D Shirt Designer</h1>
+		{/* Aquí irá tu componente Canvas */}
+	  </div>
+	)
+  }
